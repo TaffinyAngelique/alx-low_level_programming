445298c03983167ea25e1f04e6a970d0515d88fc
taffiny <taffinyangel@yahoo.com>
2023-02-16 16:08:27 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - A program that prints a line with puts function
+ * Return: 0 (if it return 0, it is a success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
